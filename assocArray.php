e2bd79ed15e3ea573c8d57c860c9dd3e400671fe
Meranz <2020813654@student.uitm.edu.my>
2022-06-08 01:37:07 +0800

    assocArray.php

--- assocArray.php
+++ assocArray.php
@@ -1,31 +1,38 @@
-?>
-
-<?php
-$monthDays = range(4,30);
-print_r ($monthDays); 
-foreach($monthDays as $key => $day30)
-{
-   if($day30 == 30)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $monthDays = array ('January' => 31, 'February' => 28,
+    'March' => 31, 'April' => 30,
+    'May' => 31, 'June' => 30,
+    'July' => 31, 'August' => 31,
+    'September' => 30, 'October' => 31,
+    'November' => 30, 'December' => 31);
+    
+    foreach ($monthDays as $key => $day)
+    {
+        echo "$key $day <br>";
+    }
+        echo "<br>";
+    ?>
+    
+    <?php
+   foreach($monthDays as $key => $day30)
    {
-       echo "$key $day30 <br>";
+       if($day30 == 30)
+       {
+           echo "$key $day30 <br>";
+       }
    }
-}
-?>
-
-<?php
-function assign_key() { 
-return 'd'; 
-} 
-$month['January'] = 1; 
-$month['b'] = 2; 
-$month['c'] = 3; 
-$month[assign_key()] = 4;
-$month['b'] = $month['a'] + $month['c'];
-
-?>
-
-
-
+    ?>
+    
+    
 
 </body>
-</html> 
+</html>
